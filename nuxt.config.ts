fd9009871c1e13956ddc0438fbeb85c433a18e62
Grant Linville <grant@acorn.io>
2024-06-18 10:15:23 -0400

    fixes
    Signed-off-by: Grant Linville <grant@acorn.io>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     'nuxt-scheduler'
   ],
   runtimeConfig: {
-    scriptPath: 'story-book.gpt', // NUXT_SCRIPT_PATH
+    scriptPath: '/app/story-book.gpt', // NUXT_SCRIPT_PATH
     storiesVolumePath: 'stories', // NUXT_STORIES_VOLUME_PATH
     gptscriptCachePath: '', // NUXT_GPTSCRIPT_CACHE_PATH
     public: {
